e6b4e70ead04f21a540a5964892833696c9c5b9e
mj <pmj2020@qq.com>
2023-09-05 15:32:59 +0800

    change shell

--- build.sh
+++ build.sh
@@ -5,9 +5,6 @@ export GOPROXY=direct
 
 sudo apt-get update
 sudo apt-get install gcc-mingw-w64-i686 gcc-multilib
-#env GOOS=windows GOARCH=386 CGO_ENABLED=1 CC=i686-w64-mingw32-gcc go build -ldflags "-s -w -extldflags -static -extldflags -static" -buildmode=c-shared -o bin/npc_sdk.dll cmd/npc/sdk.go
-#env GOOS=linux GOARCH=386 CGO_ENABLED=1 CC=gcc go build -ldflags "-s -w -extldflags -static -extldflags -static" -buildmode=c-shared -o bin/npc_sdk.so cmd/npc/sdk.go
-#tar -czvf bin/npc_sdk.tar.gz npc_sdk.dll npc_sdk.so npc_sdk.h
 
 
 ###################################-NPC-#########################################
