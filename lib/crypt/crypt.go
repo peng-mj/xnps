bf9118173a20e8f3d9d08ae535099ed7736efbcc
mj <pmj2020@qq.com>
2023-09-06 20:04:15 +0800

    将vkey生成方式修改为ulid+6位随机数组装成32位字符串

--- lib/crypt/crypt.go
+++ lib/crypt/crypt.go
@@ -5,11 +5,10 @@ import (
 	"crypto/aes"
 	"crypto/cipher"
 	"crypto/md5"
-	"crypto/sha256"
 	"encoding/hex"
 	"errors"
+	"github.com/oklog/ulid/v2"
 	"math/rand"
-	"strconv"
 	"time"
 )
 
@@ -76,18 +75,10 @@ func GetRandomString(l int) string {
 	}
 	return string(result)
 }
+
 func GenerateRandomVKey() string {
-	str := `0123456789abcdefghijklmnopqrstuvwxyzQWERTYUIOPASDFGHJKLZXCVBNM@#$<>!|+=-_*^&%`
-	bts := []byte(str)
-	var result []byte
-	r := rand.New(rand.NewSource(time.Now().UnixNano()))
-	for i := 0; i < 20; i++ {
-		result = append(result, bts[r.Intn(len(bts))])
-	}
-	pre := strconv.FormatInt(time.Now().UnixMicro(), 10)
-	str = string(result) + "_" + pre
-	sh := sha256.New()
-	sh.Write([]byte(str))
-	co := hex.EncodeToString(sh.Sum(nil))
-	return co
+
+	entropy := ulid.Monotonic(rand.New(rand.NewSource(time.Now().UnixNano())), 0)
+	out := ulid.MustNew(ulid.Timestamp(time.Now()), entropy).String() + GetRandomString(6)
+	return out
 }
