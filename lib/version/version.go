15cfc5d05b98a10451937bb54376be4c4d30a6e9
mj <pmj2020@qq.com>
2023-09-09 23:39:24 +0800

    由于修改了密钥验证方式，和原本的验证不兼容，所以，特将内核版本号修改为0.27.0

--- lib/version/version.go
+++ lib/version/version.go
@@ -1,10 +1,11 @@
 package version
 
-const VERSION = "0.26.14"
+const VERSION = "0.27.1"
 
+// 由于-验证算法修改，由md5修改为，所以原本的nps已经不兼容，特将版本号修改为”0.27.0“
 // Compulsory minimum version, Minimum downward compatibility to this version
 // Core Version
 // The Core Version of npc and nps should be eq eatch other
 func GetCoreVersion() string {
-	return "0.26.0"
+	return "0.27.0"
 }
