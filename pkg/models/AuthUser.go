14cb5bb1ec9d00661ac9ab47bdc6a0f05400db40
MJ <pmj2020@qq.com>
2024-05-11 22:07:41 +0800

    auto fid user's uid

--- pkg/models/AuthUser.go
+++ pkg/models/AuthUser.go
@@ -1,7 +1,10 @@
 package models
 
 import (
+	"gorm.io/gorm"
+	"math/rand"
 	"time"
+	"tunpx/pkg/crypt"
 )
 
 type AuthUser struct {
@@ -9,7 +12,7 @@ type AuthUser struct {
 	Uid          int64     `gorm:"column:uid;type:bigint(20);not null;unique;index" json:"uid,omitempty"`
 	Level        int32     `gorm:"column:level;type:int;not null;default:99" json:"level,omitempty"`
 	Username     string    `gorm:"column:username;type:varchar(30);not null;default:guest" json:"username"`
-	Password     string    `gorm:"column:password;type:varchar(128);not null;default:admin" json:"password,omitempty"`
+	Password     string    `gorm:"column:password;type:varchar(128);not null;default:12345" json:"password,omitempty"`
 	Emile        string    `gorm:"column:emil;type:varchar(30);not null;default:admin@tunps.com" json:"emile,omitempty"`
 	EmileAuthKey string    `gorm:"column:emil_auth_key;type:varchar(40);not null;default:empty" json:"emile_auth_key"` // SMTP or other
 	EmileEnable  bool      `gorm:"column:emil_enable;not null;default:false" json:"emile_enable,omitempty"`
@@ -28,4 +31,18 @@ type AuthUser struct {
 
 func (*AuthUser) TableName() string {
 	return "auth_user"
+}
+
+func (m *AuthUser) BeforeCreate(tx *gorm.DB) (err error) {
+	if m.Uid == 0 {
+		m.Uid = crypt.SnowID(rand.Int63n(1024))
+	}
+	m.CreateAt = time.Now()
+	m.LastLoginAt = time.Now()
+	return
+}
+
+func (m *AuthUser) BeforeUpdate(tx *gorm.DB) (err error) {
+	m.LastLoginAt = time.Now()
+	return
 }
